1fab24083495ce51288c9c7d2dc094637a309981
Emir Muñoz <emir.munoz@gmail.com>
2016-07-18 11:33:31 +0100

    Add outlier detection methods enum

---
+++ src/main/java/org/rosa/ml/numeric/DetectionType.java
@@ -0,0 +1,18 @@
+package org.rosa.ml.numeric;
+
+/**
+ * Enum for outlier detection methods.
+ *
+ * @author Emir Munoz
+ * @version 0.0.1
+ * @ since 15/07/2016
+ */
+public enum DetectionType {
+
+    ESD,
+    HAMPEL,
+    BOXPLOT,
+    ZSCORE,
+    ROBUSTZSCORE
+
+}
