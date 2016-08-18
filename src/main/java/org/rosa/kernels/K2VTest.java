b6eaf1a5a44c9851c8f14a8af3c335fb00f1e843
Emir Muñoz <emir.munoz@gmail.com>
2016-08-18 13:50:16 +0100

    RDF graph kernels test

---
+++ src/main/java/org/rosa/kernels/K2VTest.java
@@ -0,0 +1,37 @@
+package org.rosa.kernels;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.data2semantics.mustard.rdf.LiteralType;
+import org.data2semantics.mustard.rdf.RDFDataSet;
+import org.data2semantics.mustard.rdf.RDFFileDataSet;
+import org.data2semantics.mustard.rdf.RDFUtils;
+import org.data2semantics.mustard.weisfeilerlehman.StringLabel;
+import org.data2semantics.mustard.weisfeilerlehman.WeisfeilerLehmanDTGraphIterator;
+import org.data2semantics.mustard.weisfeilerlehman.WeisfeilerLehmanIterator;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.nodes.DTGraph;
+import org.nodes.DTNode;
+
+import java.util.List;
+
+/**
+ * @author Emir Munoz
+ * @version 0.0.1
+ * @since 18/08/2016
+ */
+public class K2VTest {
+
+    public static void main(String... args) {
+        String rdf_file = args[0];
+        RDFDataSet tripleStore = new RDFFileDataSet(rdf_file, RDFFormat.NTRIPLES);
+        // RDFDataSet tripleStore = new RDFFileDataSet(AIFB_FILE2, RDFFormat.NQUADS);
+        DTGraph<String, String> graph = RDFUtils.statements2Graph(
+                Sets.newHashSet(tripleStore.getFullGraph()),
+                LiteralType.REGULAR_LITERALS);
+        for (DTNode<String, String> n : graph.nodes()) {
+            System.out.println(n);
+        }
+    }
+
+}
